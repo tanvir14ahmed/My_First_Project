f0ff7abc3bcf506b8f2174d0ecd3567bba8d76e6
Tanvir Ahmed <tanvir14ahmed@gmail.com>
2023-05-07 16:41:39 +0600

    7th May 2023

--- first_app/views.py
+++ first_app/views.py
@@ -1,86 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
-from first_app.models import Musician, Album
-from first_app import forms
-from django.db.models import Avg
-
-# Create your views here.
-def index(request):
-    musician_list = Musician.objects.order_by("first_name")
-    diction = {"title":"Home Page", "musician_list":musician_list}
-    return render(request, "first_app/index.html", context = diction)
-
-def album_list(request, artist_id):
-    artist_info = Musician.objects.get(pk=artist_id)
-    album_list = Album.objects.filter(artist=artist_id).order_by("name")
-    artist_rating = Album.objects.filter(artist=artist_id).aggregate(Avg('num_stars'))
-
-    diction = {"title":"List of Albums", "artist_info":artist_info, "album_list":album_list, "artist_rating":artist_rating}
-    return render(request, "first_app/album_list.html", context = diction)
-
-def musician_form(request):
-    form = forms.MusicianForm()
-
-    if request.method == "POST":
-        form = forms.MusicianForm(request.POST)
-        if form.is_valid():
-            form.save(commit=True)
-            return index(request)
-
-    diction = {"title":"Add Musician", "musician_form":form}
-    return render(request, "first_app/musician_form.html", context = diction)
-
-def album_form(request):
-    form = forms.AlbumForm()
-
-    if request.method == "POST":
-        form = forms.AlbumForm(request.POST)
-        if form.is_valid():
-            form.save(commit = True)
-            return index(request)
+from django.views.generic import View, TemplateView
 
 
-    diction = {"title":"Add Album", "album_form":form}
-    return render(request, "first_app/album_form.html", context = diction)
+class IndexView(TemplateView):
+    template_name = 'first_app/index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["sample_text_1"] = "First Text"
+        context["sample_text_2"] = "Second Text"
+        return context
 
-def edit_artist(request, artist_id):
-    artist_info = Musician.objects.get(pk=artist_id)
-    form = forms.MusicianForm(instance=artist_info)
+class ContactPage(TemplateView):
+    template_name = 'first_app/contact.html'
 
-    if request.method == "POST":
-        form = forms.MusicianForm(request.POST, instance = artist_info)
-        if form.is_valid():
-            form.save(commit = True)
-            return album_list(request, artist_id)
-
-
-    diction ={'edit_form':form}
-    return render(request, "first_app/edit_artist.html", context = diction)
-
-
-def edit_album(request, album_id):
-    album_info = Album.objects.get(pk=album_id)
-    form = forms.AlbumForm(instance=album_info)
-    diction = {}
-
-    if request.method == "POST":
-        form = forms.AlbumForm(request.POST, instance=album_info)
-        if form.is_valid():
-            form.save(commit=True)
-            diction.update({'success_text':'Data Successfully Updated'})
-    diction.update({"edit_form":form})
-    diction.update({"album_id":album_id})
-    return render(request, "first_app/edit_album.html", context = diction)
-
-def delete_album(request, album_id):
-    album = Album.objects.get(pk=album_id).delete()
-    diction = {"delete_success":"Album Successfully Deleted !"}
-    return render(request, "first_app/delete.html", context=diction)
-
-
-def delete_musician(request, artist_id):
-    artist = Musician.objects.get(pk=artist_id).delete()
-    diction = {"delete_success":"Musician Successfully Deleted !"}
-    return render(request, "first_app/delete.html", context=diction)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["intro"] = "This is our contact page"
+        return context
