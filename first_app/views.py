38651a863a5ec2db09aef86bdeb8159968f6b088
Tanvir Ahmed <tanvir14ahmed@gmail.com>
2023-05-08 23:35:33 +0600

    08 May 2023

--- first_app/views.py
+++ first_app/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView
+from first_app import models
 
-
-class IndexView(TemplateView):
+class IndexView(ListView):
+    context_object_name = 'musician_list'
+    model = models.Musician
     template_name = 'first_app/index.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["sample_text_1"] = "First Text"
-        context["sample_text_2"] = "Second Text"
-        return context
+    
 
 class ContactPage(TemplateView):
     template_name = 'first_app/contact.html'
