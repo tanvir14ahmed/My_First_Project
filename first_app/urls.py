febc37ac5aa7d7e852cc586cc865acfe855b1b1d
Tanvir Ahmed <tanvir14ahmed@gmail.com>
2023-05-11 01:09:45 +0600

    11th May 2023

--- first_app/urls.py
+++ first_app/urls.py
@@ -7,6 +7,7 @@ app_name = "first_app"
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('contact/', views.ContactPage.as_view(), name='contact'),
-    path('musician_details/<pk>/', views.MusicianDetail.as_view(), name='musician_details')
+    path('musician_details/<pk>/', views.MusicianDetail.as_view(), name='musician_details'),
+    path('add_musician/', views.AddMusician.as_view(), name='add_musician'),
 
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import View, TemplateView, ListView, DetailView, DetailView
+from django.views.generic import View, TemplateView, ListView, DetailView, DetailView, CreateView
 from first_app import models
 
 class IndexView(ListView):
@@ -21,3 +21,8 @@ class MusicianDetail(DetailView):
     model = models.Musician
     context_object_name = 'musician'
     template_name = 'first_app/musician_details.html'
+
+class AddMusician(CreateView):
+    fields = ('first_name', 'last_name', 'instrument')
+    model = models.Musician
+    template_name = 'first_app/musician_form.html'
